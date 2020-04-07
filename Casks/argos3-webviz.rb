1b226b6a19386f45a355014b8bc898f4b4815c06
Prajankya <prajankya@gmail.com>
2020-04-06 22:48:32 -0400

    Hardcoded values

--- Casks/argos3-webviz.rb
+++ Casks/argos3-webviz.rb
@@ -1,20 +1,8 @@
-require 'net/http'
-require 'json'
-
-response = Net::HTTP.get(URI('https://api.github.com/repos/NESTlab/argos3-webviz/releases/latest'))
-json_data = JSON.parse(response)
-
-indx1 = response.rindex('OSX.pkg') + 7
-indx2 = response[0, indx1].rindex('"') + 1
-
-url = response[indx2, indx1 - indx2].strip # Get OSX url link
-pkg_name = url[(url.rindex('/') + 1)..-1]
-
 cask 'argos3-webviz' do
-  version json_data['tag_name']
-  sha256 :no_check
+  version '0.4.70'
+  sha256 'b0a94db35294ca26d651f344b45a09de4d4a9853ccdbcc193f394456d9f9e2e3'
 
-  url url
+  url 'https://github.com/NESTLab/argos3-webviz/releases/download/0.4.70/argos3plugin_simulator_webviz-0.4.70-x86_64-OSX.pkg'
   appcast 'https://github.com/NESTlab/argos3-webviz/releases.atom'
   name 'argos3-webviz'
   homepage 'https://github.com/NESTlab/argos3-webviz'
@@ -24,7 +12,7 @@ cask 'argos3-webviz' do
   depends_on formula: 'ilpincy/argos3/argos3'
   depends_on formula: 'openssl@1.1'
 
-  pkg pkg_name
+  pkg 'argos3plugin_simulator_webviz-0.4.70-x86_64-OSX.pkg'
 
   uninstall pkgutil: 'com.argos3.plugins.webviz'
 end
